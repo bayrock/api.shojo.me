c52520ddad0d7e56236c544df82b08c3e595e59a
Serene <bayrock@users.noreply.github.com>
2025-08-26 06:18:48 -0400

    fix: blob token error

--- api/refresh/monkeytype.js
+++ api/refresh/monkeytype.js
@@ -1,9 +1,11 @@
 import { put } from "@vercel/blob";
 
+const UID = 'wwezGtenkPeTFJTioEK0KZcXWXq1';
+
 export default async function handler(req, res) {
     try {
         // Fetch Monkeytype data
-        const qwertyRes = await fetch("https://api.monkeytype.com/users/wwezGtenkPeTFJTioEK0KZcXWXq1/profile?isUid")
+        const qwertyRes = await fetch(`https://api.monkeytype.com/users/${UID}/profile?isUid`)
 
         if (!qwertyRes.ok) return res.status(500).json({ error: "Failed to fetch Monkeytype data ❎" });
 
@@ -20,7 +22,8 @@ export default async function handler(req, res) {
         // Deploy monkeytype.json
         await put("monkeytype.json", JSON.stringify(output, null, 2), {
             access: "public",
-            contentType: "application/json"
+            contentType: "application/json",
+            token: process.env.BLOB_READ_WRITE_TOKEN
         });
 
         return res.status(200).json({ message: "Monkeytype data refreshed ✅" });
