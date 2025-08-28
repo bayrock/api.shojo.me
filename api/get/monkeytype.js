29d688338ab37de67a17f451b3867b2f91e44325
Serene <bayrock@users.noreply.github.com>
2025-08-28 01:29:41 -0400

    fix: cross origin request error

--- api/get/monkeytype.js
+++ api/get/monkeytype.js
@@ -6,6 +6,14 @@ const SERVER_CACHE_LIMIT = Math.floor(Number(process.env.RATE_LIMIT_MS) / 1000);
 
 export default async function handler(req, res) {
     try {
+        // Add CORS headers
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+        if (req.method === "OPTIONS") return res.status(200).end();
+
+        // Fetch data
         const data = await get(FILENAME);
         res.setHeader("Content-Type", "application/json");
         res.setHeader("Cache-Control", `public, max-age=${CLIENT_CACHE_LIMIT}, s-maxage=${SERVER_CACHE_LIMIT}`);
